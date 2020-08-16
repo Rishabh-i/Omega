73e5be71f689b47279e2ae25073aa026d55ddbad
Rishabh-i <69755978+Rishabh-i@users.noreply.github.com>
2020-08-16 18:13:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCKzgHb14f-_3CR8PMaNh7C44ru6f5uLDk",
+  authDomain: "instagram-2a137.firebaseapp.com",
+  databaseURL: "https://instagram-2a137.firebaseio.com",
+  projectId: "instagram-2a137",
+  storageBucket: "instagram-2a137.appspot.com",
+  messagingSenderId: "178865935409",
+  appId: "1:178865935409:web:0016d9439a7ebb1269f110",
+  measurementId: "G-XFCES055HW"
 };
 
      
